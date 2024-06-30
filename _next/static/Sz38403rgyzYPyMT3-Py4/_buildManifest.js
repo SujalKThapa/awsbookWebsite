b5d1f362ac341cfe3d156a84d38e1f6c8efc7094
SujalKThapa <136220535+SujalKThapa@users.noreply.github.com>
2024-07-01 01:57:20 +0530

    newStufff

---
+++ _next/static/Sz38403rgyzYPyMT3-Py4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/3f06fcd6-96ad633e70ae1ca2.js","static/css/2029939e1fecbdc6.css","static/chunks/pages/index-7e05cd72fd4934ce.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
